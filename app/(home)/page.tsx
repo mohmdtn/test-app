761a257d2a74b58566019e7c764f23b7025c1e9e
mohmdtn <mohammad.sawdegh@gmail.com>
2023-12-20 22:54:29 +0330

    add chats button

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,15 +3,20 @@ import CreateAccount from '../components/CreateAccount'
 import Navbar from '../components/Navbar'
 import Table from '../components/Table'
 
+import { PiChatsCircleLight } from "react-icons/pi"
+
 export default function Home() {
   return (
-    <main >
+    <section className='relative'>
       <Navbar />
       <section className='px-8'>
         <BreadCrumb />
         <CreateAccount />
         <Table />
       </section>
-    </main>
+      <div className='absolute bottom-0 cursor-pointer left-4 w-12 h-12 rounded-full border border-yellow-600 bg-yellow-200 flex justify-center items-center hover:shadow-lg duration-150'>
+        <PiChatsCircleLight size={20} className={"text-yellow-600"} />
+      </div>
+    </section>
   )
 }
